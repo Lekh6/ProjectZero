15961aa8c92d1de83146257b8cbc0f6107e001f6
leka <lekharuthwik262@gmail.com>
2025-10-29 23:23:56 +0530

    day2

--- app.py
+++ app.py
@@ -2,17 +2,21 @@ from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 
+app = Flask(__name__)
+
 app.config["SESSION_PERMANENT"] = True
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-app = Flask(__name__)
+if __name__ == "__main__":
+    app.run(debug=True)
+
 
 @app.route('/login')
 def login():
-    return render_template('layout.html')
+    return render_template('login.html')
 
-@login_required
+
 @app.route("/")
 def homepage():
-    
+    return redirect('/login')
